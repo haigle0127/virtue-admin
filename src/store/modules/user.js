afa8ea1cf32f0b2d5881f2111d9852de86c7eed1
haigle <991382548@qq.com>
2020-12-29 10:57:54 +0800

    token验证更换为实体

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,8 +35,8 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ account: account.trim(), password: password }).then(response => {
         const { data } = response
-        commit('SET_TOKEN', data)
-        setToken(data)
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
         resolve()
       }).catch(error => {
         console.log(error)
